ad0db901d6ed25985da44da5e45cb0109ff14e61
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2023-10-25 23:25:26 +0100

    Various CSS improvements

--- static/loadTasks.js
+++ static/loadTasks.js
@@ -90,7 +90,7 @@ function generateTaskDiv(taskName, taskPath, taskDetails = "") {
         anchor.href = url
         anchor.target = "_blank"
 
-        anchor.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="1em" style="vertical-align: middle; margin-left: 2px;">
+        anchor.innerHTML = `<svg class="QuickLink" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="1em" style="vertical-align: middle; margin-left: 2px;">
         <path
             d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
         </svg>`
